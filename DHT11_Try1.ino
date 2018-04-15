80d300c47221aa91929e326626a8904057a01819
jatinkeshav24 <37905200+jatinkeshav24@users.noreply.github.com>
2018-04-15 10:49:54 +0530

    Add files via upload

---
+++ DHT11_Try1.ino
@@ -0,0 +1,82 @@
+double exiting, entring;
+#define DataLine 7
+uint8_t data = 0, Hum_High = 0, Hum_Low = 0, Temp_High = 0, Temp_Low = 0, Checksum = 0;
+void setup()
+  {
+    Serial.begin(9600);
+    Serial.println("::::::::::::::::::::::::Weather Station::::::::::::::::");
+    delay(2000);
+  }
+
+void loop()
+  {
+    Request();
+    Response();
+    Hum_High = RecData();
+    Hum_Low = RecData();
+    Temp_High = RecData();
+    Temp_Low = RecData();
+    Checksum = RecData();
+  if ((Hum_High + Hum_Low + Temp_High + Temp_Low) != Checksum)
+        Serial.println("Error");
+  else
+    {
+        Serial.print("Humidity:");
+        Serial.print(Hum_High);
+        Serial.print(".");
+        Serial.print(Hum_Low);
+        Serial.println("%");
+        Serial.print("Temprature:");
+        Serial.print(Temp_High);
+        Serial.print(".");
+        Serial.print(Temp_Low);
+        Serial.println(" C");
+        Serial.println("===============================");
+    }
+
+  delay(2000);
+    }
+
+//Function definitions
+
+void Request()
+{
+  pinMode(DataLine, OUTPUT);
+  digitalWrite(DataLine, LOW);
+  delay(22);//Wait For more than 18ms
+  digitalWrite(DataLine, HIGH);
+  delayMicroseconds(28);
+}
+
+void Response()
+{
+  pinMode(DataLine, INPUT);
+  while ((digitalRead(DataLine) == HIGH)); //Change of Signal... Pulled High
+  while ((digitalRead(DataLine) == LOW)); //80uS
+  while ((digitalRead(DataLine) == HIGH)); //80uS: So if this ends , this means that its definitly LOW on DataLine Now
+
+}
+
+uint8_t RecData()
+{
+  int i;
+  for (i = 0; i < 8; i++)
+  {
+    while ((digitalRead(DataLine) == LOW));
+    delayMicroseconds(30);
+    if ((digitalRead(DataLine) == HIGH))
+      //Its a HIGH
+    {
+      data = data << 1; //MSB First
+      data = data | 0x01; //Writing to the last bit
+    }
+    else
+      // LOW Case
+      data = data << 1;
+    while ((digitalRead(DataLine) == HIGH)); //Doesn't Matter How Long it Goes ..(Bad Programming)
+  }
+  return data;
+}
+
+
+
---
+++ For Atmega.c
@@ -0,0 +1,98 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lcd_io.h"
+#include "util/delay.h"
+
+#define DHT11_PIN 6
+uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
+
+void Request()					/* Microcontroller send start pulse or request */
+{
+	DDRD |= (1<<DHT11_PIN);
+	PORTD &= ~(1<<DHT11_PIN);	/* set to low pin */
+	_delay_ms(20);				/* wait for 20ms */
+	PORTD |= (1<<DHT11_PIN);	/* set to high pin */
+}
+
+void Response()					/* receive response from DHT11 */
+{
+	DDRD &= ~(1<<DHT11_PIN);
+	while(PIND & (1<<DHT11_PIN));
+	while((PIND & (1<<DHT11_PIN))==0);
+	while(PIND & (1<<DHT11_PIN));
+}
+
+uint8_t Receive_data()			/* receive data */
+{	
+	int q;
+	for ( q=0; q<8; q++)
+	{
+		while((PIND & (1<<DHT11_PIN)) == 0);	/* check received bit 0 or 1 */
+		_delay_us(30);
+		if(PIND & (1<<DHT11_PIN))	 /* if high pulse is greater than 30ms */
+		c = (c<<1)|(0x01);		     /* then its logic HIGH */
+		else				         /* otherwise its logic LOW */
+		c = (c<<1);
+		while(PIND & (1<<DHT11_PIN));
+	}
+	return c;
+}
+
+int main(void)
+{	
+	char data[5];
+	
+    lcd_init();					/* initialize LCD */
+	lcd_clrscr();			/* clear LCD */
+	lcd_gotoxy(0,0);			/* enter column and row position */
+	lcd_puts("Humidity:");
+	//lcd_gotoxy(1,0);
+	//lcd_puts("Temp = ");
+	
+    while(1)
+	{	
+		Request();		                       /* send start pulse */
+		Response();			                   /* receive response */
+		I_RH=Receive_data();	               /* store first eight bit in I_RH */
+		D_RH=Receive_data();	               /* store next eight bit in D_RH */
+		I_Temp=Receive_data();                 /* store next eight bit in I_Temp */
+		D_Temp=Receive_data();	               /* store next eight bit in D_Temp */
+		CheckSum=Receive_data();               /* store next eight bit in CheckSum */
+		
+		if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum)
+		{
+			lcd_gotoxy(0,0);
+			lcd_puts("Error");
+		}
+		
+		else
+		{	
+			itoa(I_RH,data,10);
+			lcd_gotoxy(11,0);
+			lcd_puts(data);
+			lcd_puts(".");
+			
+			itoa(D_RH,data,10);
+			lcd_puts(data);
+			//lcd_puts("\%");
+
+			//itoa(I_Temp,data,10);
+			//lcd_gotoxy(8,1);
+			//lcd_puts(data);
+			//lcd_putc('.');
+			
+			//itoa(D_Temp,data,10);
+			//lcd_puts(data);
+			//lcd_putc(0xDF);
+			//lcd_putc('C');
+			
+			//itoa(CheckSum,data,10);
+			//lcd_puts(data);
+			//lcd_putc(' ');
+		}
+				
+	_delay_ms(10);
+	}	
+}
